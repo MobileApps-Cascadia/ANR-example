07f141593c466e8252ef5d1f7cf7556a0a9f3366
Brian Bansenauer <brianb@cascadia.edu>
2018-10-24 21:33:11 -0700

    updated with seekbar

--- app/src/main/java/edu/cascadia/brianb/anr_example/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/anr_example/MainActivity.java
@@ -3,6 +3,7 @@ package edu.cascadia.brianb.anr_example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,19 +13,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Button click to sleep the UI thread
         //Continue clicking on the app while "hung" to see the ANR message
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener()
+
+        final SeekBar secondsSeek = (SeekBar)findViewById(R.id.sleepMeSeek);
+        findViewById(R.id.stopButton).setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(final View v)
             {
-                SleepAMinute();
+                SleepAwhile(secondsSeek.getProgress());
             }
         });
 
     }
-    private static void SleepAMinute() {
+    private static void SleepAwhile(int sec) {
         try {
-            Thread.sleep(20 * 1000); // sleep for 20 seconds
+            Thread.sleep(sec * 1000); // sleep for 2 seconds
         }
         catch (InterruptedException e) {
             e.printStackTrace();
