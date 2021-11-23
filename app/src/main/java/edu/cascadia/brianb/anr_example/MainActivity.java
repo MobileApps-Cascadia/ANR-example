de729ac0f6e3e596b1ea5437806cdaca39b9b3ff
Brian Bansenauer <brianb@cascadia.edu>
2021-11-23 12:58:01 -0800

    adds threads via click - check profiler>CPU

--- app/src/main/java/edu/cascadia/brianb/anr_example/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/anr_example/MainActivity.java
@@ -12,40 +12,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Button click to sleep the UI thread
-        //Continue clicking on the app while "hung" to see the ANR message
 
         final SeekBar secondsSeek = findViewById(R.id.sleepMeSeek);
         final TextView displayDelay = findViewById(R.id.displayDelay);
         final Button stallButton = findViewById(R.id.stallButton);
 
+        //Handles SeekBar update
         secondsSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 displayDelay.setText(getString(R.string.delayDisplay, progress));
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+
+        //Handles Button click
         stallButton.setOnClickListener(view -> {
-            new Thread(new Runnable() {
-                public void run() {
-                    view.post(new Runnable() {
-                        public void run() {
-                            try {
-                                Thread.sleep(secondsSeek.getProgress() * 1000); // sleep for some seconds
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
+            new Thread(() -> {
+                try {
+                    Thread.sleep(secondsSeek.getProgress() * 1000); // sleep for some seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }).start();
         });
