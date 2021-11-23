166fabe3c3eecfc7746f078111c6a4d892759477
Brian Bansenauer <brianb@cascadia.edu>
2021-11-23 12:08:07 -0800

    updates gradle

--- app/src/main/java/edu/cascadia/brianb/anr_example/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/anr_example/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.cascadia.brianb.anr_example;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,20 +16,29 @@ public class MainActivity extends AppCompatActivity {
         //Button click to sleep the UI thread
         //Continue clicking on the app while "hung" to see the ANR message
 
-        final SeekBar secondsSeek = (SeekBar)findViewById(R.id.sleepMeSeek);
-        findViewById(R.id.stopButton).setOnClickListener(new View.OnClickListener()
-        {
+        final SeekBar secondsSeek = findViewById(R.id.sleepMeSeek);
+        final TextView displayDelay = findViewById(R.id.displayDelay);
+        final Button stallButton = findViewById(R.id.stallButton);
+
+        secondsSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(final View v)
-            {
-                SleepAwhile(secondsSeek.getProgress());
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                displayDelay.setText(getString(R.string.delayDisplay,progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+        stallButton.setOnClickListener( view -> SleepAwhile(secondsSeek.getProgress()));
 
     }
     private static void SleepAwhile(int sec) {
         try {
-            Thread.sleep(sec * 1000); // sleep for 2 seconds
+            Thread.sleep(sec * 1000); // sleep for some seconds
+
         }
         catch (InterruptedException e) {
             e.printStackTrace();
